91962fe147145456a3dbc2e19b1ae5c7f690c71e
hongdonghyun <hong4367@gmail.com>
2020-06-29 15:05:52 +0900

    UITest 추가 -> 사파리에서 앱으로 되돌아오기, 디테일뷰 테스트

--- Hong3Store/Hong3StoreUITests/Hong3StoreAppViewUITests.swift
+++ Hong3Store/Hong3StoreUITests/Hong3StoreAppViewUITests.swift
@@ -10,6 +10,7 @@ import XCTest
 
 class Hong3StoreAppViewUITests: XCTestCase {
     var app: XCUIApplication!
+    var safari: XCUIApplication!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,6 +20,8 @@ class Hong3StoreAppViewUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         app = XCUIApplication()
+        safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafati")
+        
         app.launch()
         super.setUp()
     }
@@ -26,6 +29,7 @@ class Hong3StoreAppViewUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         app = nil
+        safari = nil
     }
 
     func test_success_swipeLeftRight_appViewcells() throws {
@@ -85,7 +89,6 @@ class Hong3StoreAppViewUITests: XCTestCase {
         topgrossCell.tap()
         XCTAssertTrue(app.staticTexts["detailTitleLabel"].waitForExistence(timeout: 1))
         app.navigationBars.buttons.element(boundBy: 0).tap()
-
     }
     
     func test_success_moreBtn_tap_topgrossView() {
@@ -98,12 +101,43 @@ class Hong3StoreAppViewUITests: XCTestCase {
         
     }
     
+    func test_success_moreBtn_scroll_topgrossView() {
+        let header = app.tables.otherElements["headerView"].firstMatch
+        header.buttons.element(matching: .any, identifier: "showAll").tap()
+        app.swipeUp()
+        app.swipeUp()
+        app.swipeUp()
+        app.swipeDown()
+        app.swipeDown()
+        app.swipeDown()
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+    }
+    
     func test_success_display_detailView() {
+        
         let preViewCell = app.tables.cells["PreviewTableCell"]
         preViewCell.tap()
+        app.buttons["downloadBtn"].firstMatch.tap()
+        // iOS 11
+        _ = safari.wait(for: .runningForeground, timeout: 5)
+        app.activate()
+        
         app.buttons.element(matching: .any, identifier: "detailMoreBtn").tap()
+        
         XCTAssertTrue(app.staticTexts["detailTitleLabel"].waitForExistence(timeout: 1))
         XCTAssertTrue(app.staticTexts["새로운 기능"].waitForExistence(timeout: 1))
+        
+        app.swipeUp()
+        
+        app.staticTexts["PreviewCollectionView"].swipeLeft()
+        app.staticTexts["PreviewCollectionView"].swipeLeft()
+        app.staticTexts["PreviewCollectionView"].swipeRight()
+        app.staticTexts["PreviewCollectionView"].swipeRight()
+        
+        addTeardownBlock {
+            self.safari = nil
+        }
+        
     }
     
 //    func test_success_moreBtn_tap_detailView() {
